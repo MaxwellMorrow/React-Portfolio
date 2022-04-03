08b92b52d8b4f309a4afd70852a9e7495ff199c8
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-04-03 14:13:14 -0700

    added some inline styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,32 @@
 import React from 'react'
 
+// CSS inside javascript 
+import styled from 'styled-components'
+
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <Navbar>
+      <Navitem>Home</Navitem>
+      <Navitem>About</Navitem>
+      <Navitem>Projects</Navitem>
+      <Navitem>Contact Me</Navitem>
+    </Navbar>
+  );
 }
 
+
+const Navbar = styled.div`
+background-color:red;
+color:white;
+position:sticky;
+display:flex;
+justify-content:flex-end;`;
+
+const Navitem = styled.div`
+padding-left:10px;
+padding-right:10px;
+&:hover{
+  color:black
+};
+`;
 export default Header
