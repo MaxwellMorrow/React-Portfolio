321e0431c8c811b1bfe2a344f5846ef4ddd33968
Maxwell Morrow <maxdmorrow@yahoo.com>
2022-04-03 13:29:54 -0700

    setup with basic left,right,header,main

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Main(props) {
+    let name = props.name
+  return (
+    <div><h1>My name is {name}</h1>
+    
+    
+    </div>
+    
+  )
+}
+
+export default Main
